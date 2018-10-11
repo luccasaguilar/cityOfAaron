8081cd67dad4f74c7a382e9d6f260940a70c1ecd
Luccas <Luccas@Luccas-PC>
2018-10-10 22:15:47 -0300

    10/10/2018 - created project

---
+++ src/cityofaaron/CityOfAaron.java
@@ -0,0 +1,22 @@
+/*
+* The main() class file for the cityOfAaron project
+* CIT-260
+* Spring 2018
+* Team members: Luccas Aguilar, Ronan Macedo, Ronald Silva
+ */
+package cityofaaron;
+
+/**
+ *
+ * @author Luccas
+ */
+public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
