cbd968c61e65428a49134ec7b37e1c5b6a764c51
Luccas <Luccas@Luccas-PC>
2018-10-26 01:43:10 -0200

    10/25/2018 - CropControl (control) added plantCrops Tests

--- test/control/CropControlLuccasTest.java
+++ test/control/CropControlLuccasTest.java
@@ -33,4 +33,106 @@ public class CropControlLuccasTest {
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
     }    
+     /**
+     * Test of plantCrops method, of class CropControl.
+     */
+    @Test
+    public void testPlantCrops2() {
+        System.out.println("plantCrops2");
+        int acresToPlant = -50;
+        CropData cropData = new CropData();
+        cropData.setPopulation(100);
+        cropData.setCropYield(200);
+        cropData.setWheatInStore(400);
+        int expResult = -1;
+        int result = CropControl.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }    
+
+     /**
+     * Test of plantCrops method, of class CropControl.
+     */
+    @Test
+    public void testPlantCrops3() {
+        System.out.println("plantCrops3");
+        int acresToPlant = 100;
+        CropData cropData = new CropData();
+        cropData.setPopulation(100);
+        cropData.setCropYield(80);
+        cropData.setWheatInStore(400);
+        int expResult = -1;
+        int result = CropControl.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }    
+    
+     /**
+     * Test of plantCrops method, of class CropControl.
+     */
+    @Test
+    public void testPlantCrops4() {
+        System.out.println("plantCrops4");
+        int acresToPlant = 100;
+        CropData cropData = new CropData();
+        cropData.setPopulation(5);
+        cropData.setCropYield(200);
+        cropData.setWheatInStore(400);
+        int expResult = -1;
+        int result = CropControl.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }    
+
+     /**
+     * Test of plantCrops method, of class CropControl.
+     */
+    @Test
+    public void testPlantCrops5() {
+        System.out.println("plantCrops5");
+        int acresToPlant = 100;
+        CropData cropData = new CropData();
+        cropData.setPopulation(100);
+        cropData.setCropYield(200);
+        cropData.setWheatInStore(40);
+        int expResult = -1;
+        int result = CropControl.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }    
+
+     /**
+     * Test of plantCrops method, of class CropControl.
+     */
+    @Test
+    public void testPlantCrops6() {
+        System.out.println("plantCrops6");
+        int acresToPlant = 50;
+        CropData cropData = new CropData();
+        cropData.setPopulation(100);
+        cropData.setCropYield(50);
+        cropData.setWheatInStore(400);
+        int expResult = 50;
+        int result = CropControl.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }    
+    
+     /**
+     * Test of plantCrops method, of class CropControl.
+     */
+    @Test
+    public void testPlantCrops7() {
+        System.out.println("plantCrops7");
+        int acresToPlant = 0;
+        CropData cropData = new CropData();
+        cropData.setPopulation(100);
+        cropData.setCropYield(200);
+        cropData.setWheatInStore(400);
+        int expResult = 0;
+        int result = CropControl.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }    
+
 }
