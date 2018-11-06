378b2eaeda4e3ea9d86cff26679c1e956466cb08
Luccas <Luccas@Luccas-PC>
2018-11-05 22:59:56 -0200

    11/05/2018 - removing tests from main class;

--- src/cityofaaron/CityOfAaron.java
+++ src/cityofaaron/CityOfAaron.java
@@ -27,62 +27,7 @@ public class CityOfAaron
    
         MainMenuView mmv = new MainMenuView();
         mmv.displayMenuView();       
-
-        // TODO code application logic here
-     Player player = new Player();
-     player.setName("Ron");
-     
-     Game game = new Game();
-     game.setPlayer(player);
-     
-     System.out.println(game.getPlayer().getName());
-     
-     System.out.println(TeamMember.RON.getName()+ " : "+ TeamMember.RON.getTitle());
-     
-     /*
-     *Test for Location class
-     *@author Ronsilva
-     */
-     Location location = new Location();
-     location.setDescription("Great");
-     location.setSymbol("Mountains");
-     System.out.println(location.getDescription() + "Symbol:" + location.getSymbol());
-     
-     /*
-     *Test for LisiItem class
-     *@author Luccas
-     */
-     
-     ListItem item = new ListItem();
-     item.setName("sword");
-     item.setNumber(1);
-     System.out.println(item.getName() + " - Qtd.:" + item.getNumber());
-     
-     /*
-     *Test for CropData class
-     *@author Ronan
-     */
-     CropData crop = new CropData();
-     crop.setYear(1);
-     crop.setPopulation(250);
-     crop.setAcresOwned(10);
-     crop.setCropYield(600);
-     crop.setWheatInStore(100);
-     crop.setNumberWhoDied(100);
-     crop.setNewPeople(150);
-     crop.setHarvest(600);
-     crop.setWheatForFood(250);
-     crop.setAcresPlanted(10);
-     System.out.println("Year:" + crop.getYear());
-     System.out.println("Population:" + crop.getPopulation());
-     System.out.println("Acres Owned:" + crop.getAcresOwned());
-     System.out.println("Crop Yield:" + crop.getCropYield());
-     System.out.println("Wheat in Store:" + crop.getWheatInStore());
-     System.out.println("Number Who Died:" + crop.getNumberWhoDied());
-     System.out.println("New People:" + crop.getNewPeople());
-     System.out.println("Harvest:" + crop.getHarvest());
-     System.out.println("Wheat for Food:" + crop.getWheatForFood());
-     System.out.println("Acres Planted:" + crop.getAcresPlanted());
+    
     }
     
     public static void setGame(Game _game) {
