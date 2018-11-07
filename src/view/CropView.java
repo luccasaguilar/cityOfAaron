63fe75ef060c43a3c00bfdfcd4986995f7fcfe64
Luccas <Luccas@Luccas-PC>
2018-11-07 00:45:33 -0200

    11/05/2018 - plantCropsView() created on CropView class

--- src/view/CropView.java
+++ src/view/CropView.java
@@ -27,6 +27,7 @@ public class CropView {
     // Purpose: interface with the user input for buying land
     // Parameters: none
     // Returns: none
+    // author Luccas
     public static void buyLandView(){
         
         // Get the cost of land for this round.
@@ -47,7 +48,28 @@ public class CropView {
         System.out.format("You now own %d acres of land. ", cropData.getAcresOwned());
     }
     
-    // The runCropView method()
+    // The plantCropsView method
+    // Purpose: interface with the user input for planting land
+    // Parameters: none
+    // Returns: none  
+    // author Luccas
+    public static void plantCropsView(){
+        
+        // Prompt the user to enter the number of acres to plant
+        System.out.print("\nHow many acres of land do you want to plant? "); 
+
+        //  Get the user’s input and save it.
+        int acresToPlant;
+        acresToPlant = keyboard.nextInt();
+
+        // Call the plantCrops( ) method in the control layer to plant crops
+        CropControl.plantCrops(acresToPlant, cropData);
+        
+        // output how much land we now own
+        System.out.format("You now have %d acres planted. ", cropData.getAcresPlanted());
+    }
+
+        // The runCropView method()
     // Purpose: runs the methods to manage the crops game
     // Parameters: none
     // Returns: none
@@ -55,8 +77,12 @@ public class CropView {
         
         // call the buyLandView( ) method
         buyLandView( );
+        
+        // call the plantCropsView( ) method
+        plantCropsView();
 
         // add calls to the other crop view methods
         // as they are written
-        }
+    }
+
 }
