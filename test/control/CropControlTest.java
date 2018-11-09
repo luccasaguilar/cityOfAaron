33f8024015b0034215f8fbed6c3ceadbb228391b
Ronan Macedo Account <ronan.macedo.edu@gmail.com>
2018-11-09 17:55:25 -0200

    11/05/2018 - adjustments on DisplayPrintView for case 5

--- test/control/CropControlTest.java
+++ test/control/CropControlTest.java
@@ -17,20 +17,67 @@ public class CropControlTest {
     
     public CropControlTest() {
     }
-
+    
     /**
-     * Test of calcLandCost method, of class CropControl.
+     * Test of buyLand method, of class CropControl.
      */
     @Test
-    public void testCalcLandCost() {
-        System.out.println("calcLandCost");
-        int expResult = 0;
-        int result = CropControl.calcLandCost();
-        assertEquals(result, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        public void testBuyLand1() {
+        System.out.println("buyLand1");
+        int landPrice = 20;
+        int acresToBuy = 10;
+        CropData cropData = new CropData();
+        cropData.setWheatInStore (400);
+        int expResult = 10;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
     }
-
     
+        @Test
+        public void testBuyLand2() {
+        System.out.println("buyLand2");
+        int landPrice = 20;
+        int acresToBuy = -5;
+        CropData cropData = new CropData();
+        cropData.setWheatInStore (400);
+        int expResult = -1;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
     
+        @Test
+        public void testBuyLand3() {
+        System.out.println("buyLand3");
+        int landPrice = 20;
+        int acresToBuy = 100;
+        CropData cropData = new CropData();
+        cropData.setWheatInStore (400);
+        int expResult = -1;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+        @Test
+        public void testBuyLand4() {
+        System.out.println("buyLand4");
+        int landPrice = 20;
+        int acresToBuy = 20;
+        CropData cropData = new CropData();
+        cropData.setWheatInStore (400);
+        int expResult = 20;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
+    
+        @Test
+        public void testBuyLand5() {
+        System.out.println("buyLand5");
+        int landPrice = 20;
+        int acresToBuy = 0;
+        CropData cropData = new CropData();
+        cropData.setWheatInStore (400);
+        int expResult = 0;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+    }
 }
